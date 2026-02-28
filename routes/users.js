328020f2956ba7301959c2777573128a3cccc7ce
ravishhegde7 <ravishhegde6@gmail.com>
2026-02-28 14:34:07 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const mongoose = require('mongoose');
 const { protect, authorize } = require('../middleware/auth');
 const User = require('../models/User');
 const Rating = require('../models/Rating');
