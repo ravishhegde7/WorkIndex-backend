d19d36f5e7dfe735439488a162470cdb0c2d4170
ravishhegde7 <ravishhegde6@gmail.com>
2026-03-20 14:34:39 +0530

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -17,7 +17,7 @@ const notificationSchema = new mongoose.Schema({
   },
   type: {
     type: String,
-    enum: ['announcement', 'system', 'refund', 'approach', 'chat', 'admin_dm','customer_interest'],
+    enum: ['announcement', 'system', 'refund', 'approach', 'chat', 'admin_dm','customer_interest', 'admin_action'],
     default: 'system'
   },
   isRead: {
