2d8c220cfdfa9a6c111f31a996dadcd9c9c6676d
ravishhegde7 <ravishhegde6@gmail.com>
2026-04-03 03:51:31 +0530

    Update ServiceCategory.js

--- models/ServiceCategory.js
+++ models/ServiceCategory.js
@@ -1,4 +1,7 @@
+const mongoose = require('mongoose');
+
 const OptionSchema = new mongoose.Schema({
+  
   value: { type: String, required: true },
   label: { type: String, required: true },
   icon:  { type: String },
