c27fe4b6bca5a28e970a599d3980b9fd9797007c
ravishhegde7 <ravishhegde6@gmail.com>
2026-04-09 21:27:37 +0530

    Update credits.js

--- routes/credits.js
+++ routes/credits.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 const { protect, authorize } = require('../middleware/auth');
 const User = require('../models/User');
 const Transaction = require('../models/Transaction');
